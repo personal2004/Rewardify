df2fbfa296dbb5cdcf6847ec2b2cc491fea4a1ab
personal2004 <kowsalyakavitha64@gmail.com>
2024-09-13 19:29:00 +0530

    Created Otp Css  body

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 1. This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 2. Addes reset css in index.css
 3. Created Form Control component
+4. Created Login Page with email and Otp verification
 
 # Installation
 
@@ -13,7 +14,8 @@
 2. npm install formik
 3. npm install react-datepicker
 4. npm install react-router-dom
-4. npm install yup
+5. npm install yup
+6. npm install nodemailer
 
 # Usage
 
--- src/Components/verifyOTP/verifyOtp.jsx
+++ src/Components/verifyOTP/verifyOtp.jsx
@@ -1,21 +1,50 @@
 import './index.css';
 import { LoginLogo} from '../../img/images';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+import { Formik,Form } from 'formik';
+import FormikControl from '../formikComponent/formikControl';
+import * as Yup from 'yup';
 
 const VerifyOtp=()=>{
-    const navigate=useNavigate();
+   const initialValues={
+        otp1: '',
+        otp2: '',
+        otp3: '',
+        otp4: '',
+      }
+    const validationSchema=Yup.object({
+        otp1: Yup.string().required('Required').matches(/^\d$/, 'Must be a digit'),
+        otp2: Yup.string().required('Required').matches(/^\d$/, 'Must be a digit'),
+        otp3: Yup.string().required('Required').matches(/^\d$/, 'Must be a digit'),
+        otp4: Yup.string().required('Required').matches(/^\d$/, 'Must be a digit'),
+      })
+      const onSubmit=values => {
+        alert(JSON.stringify(values, null, 2));
+      }
+    // const navigate=useNavigate();
     return(
         <div className='login_verify'>
             <img src={LoginLogo} alt='icon'/>
             <h6>Verify your details</h6>
             <p>Enter OTP number below</p>
-            <div className='login_verify_content_input'>
-                
-            </div>
-            <div className='login_verify_content'>
-                <button>Verify and Continue</button>
-                <p>Didn’t receive OTP?  Resend in 0:55</p>
-            </div>
+            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+              {(formik)=>(
+                 <>       
+                <div className='login_verify_content'>
+                            <Form className='login_verify_content_input'>
+                            <FormikControl control='input' name='otp1' maxLength='1'type="text"/>
+                            <FormikControl control='input' name='otp2' maxLength='1'type="text"/>
+                            <FormikControl control='input' name='otp3' maxLength='1'type="text"/>
+                            <FormikControl control='input' name='otp4' maxLength='1'type="text"/>
+                            </Form>
+                </div>
+                <div className='login_verify_content_button'>
+                    <button>Verify and Continue</button>
+                    <div className='login_verify_resend'>Didn’t receive OTP?  Resend in 0:55</div>
+                </div>
+                </>
+                )}
+            </Formik>
         </div>
     );
 }
