91ca80017dde24ad511eeaa1648f2fe0482bc512
personal2004 <kowsalyakavitha64@gmail.com>
2024-10-29 14:24:46 +0530

    changed  checkbox value as string and height of Modalprepare

--- src/Components/storeCreate/storeinfo/workingdayform/workingdayForm.jsx
+++ src/Components/storeCreate/storeinfo/workingdayform/workingdayForm.jsx
@@ -19,13 +19,13 @@ const WorkdayForm=()=>{
     [days,location.pathname])
 
      const WorkdaysCheckOption=[
-        {key:'Monday',value:1},
-        {key:'Tuesday',value:2},
-        {key:'Wednesday',value:3},
-        {key:'Thursday',value:4},
-        {key:'Friday',value:5},
-        {key:'Saturday',value:6},
-        {key:'Sunday',value:7}
+        {key:'Monday',value:'1'},
+        {key:'Tuesday',value:'2'},
+        {key:'Wednesday',value:'3'},
+        {key:'Thursday',value:'4'},
+        {key:'Friday',value:'5'},
+        {key:'Saturday',value:'6'},
+        {key:'Sunday',value:'7'}
     ]
 
     return(
